78336f8c3c32318b18a973e7068c36cd2a6d4206
Dina Betser <dbetser@gmail.com>
2012-02-20 11:36:56 -0500

    ready for prime time

--- assignment1/photo_gallery.py
+++ assignment1/photo_gallery.py
@@ -1,7 +1,8 @@
 # Copyright 2012 Dina Betser.
-# Photo gallery main file for 6.170 Assignment #1.
+# Photo gallery main script for 6.170 Assignment #1.
 # Usage: photo_gallery.py <image_directory> <output_filename>
-#                         <comma-separated list of metadata fields.>
+#                         <comma-separated list of metadata fields>
+#                         <input_filename>
 
 import os
 import string
@@ -13,13 +14,15 @@ from jinja2 import FileSystemLoader
 from iptcinfo import IPTCInfo
 
 class ImageDataObject(object):
-  """Struct-like object to store all information needed to display an image."""
-
-  def __init__(self, image_path, caption, prev_path, next_path, id):
+  """Struct-like object to store all information needed to display an image.
+  
+  Used by Jinja when injecting image data into HTML.
+  """
+  def __init__(self, image_path, caption, prev, next, id):
     self.path = image_path
     self.caption = caption
-    self.prev = prev_path
-    self.next = next_path
+    self.prev = prev
+    self.next = next
     self.id = id
 
   def __repr__(self):
@@ -44,7 +47,9 @@ class PhotoGalleryManager(object):
     self.SetImageObjects()
 
   def GenerateIdFromPath(self, path):
-    return string.split(path, '.')[0]
+    """Utility function to strip the suffix from a file path."""
+    # Add text in front to ensure valid html even if name starts with a number.
+    return 'photo' + string.split(path, '.')[0]
 
   def SetImageObjects(self):
     """Generate all of the image objects; store them in the class variable."""
@@ -59,12 +64,11 @@ class PhotoGalleryManager(object):
       ))
 
   def GetImageObjects(self):
-    for obj in self.image_objs:
-      print obj
+    """Getter for the list of image objects."""
     return self.image_objs
 
   def LoadIptcDataForFile(self, filename):
-    """Retrieves the caption for the given file."""
+    """Retrieve the caption for the given file."""
     # Create new info object.
     info = IPTCInfo(filename)
   
@@ -76,15 +80,15 @@ class PhotoGalleryManager(object):
     # Get specific attributes.
     caption = ''
     for field in self.meta_fields:
-      print field, info.data[field]
       if info.data[field] is not None:
         caption += field + ': ' + info.data[field] + '.'
       else:
+        caption += "Field " + field + " was empty."
         print 'Caption could not be created for field ' + field
     return caption
 
   def SetNewMetadataFields(self, metadata_fields):
-    """Sets the metadata fields of the images to use for the caption.
+    """Set the metadata fields of the images to use for the caption.
     
     Args:
       metadata_fields: A list of the names of the metadata fields.
@@ -101,7 +105,7 @@ class JinjaAdapter(object):
     self.InitEnvironment()
 
   def InitEnvironment(self):
-    """Initializes the Jinja environment."""
+    """Initialize the Jinja environment and store the template in the class."""
     env = Environment(loader=FileSystemLoader('.'))
     self.template = env.get_template(self.input_file)
 
